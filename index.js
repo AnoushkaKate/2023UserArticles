71b29d0d588d69215387b71a809efdccd4aa03c6
KateHampton <hampton.k@maristcollege.school.nz>
2023-10-23 15:23:26 +1300

    comments

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
+// Referencing the article tile and article input elements. 
 const expandingInputTitle = document.getElementById('expanding-input-title');
 const expandingInputArticle = document.getElementById('expanding-input-article');
 
-
+// Adding event listener to the article title input field so its height expands and the user types.
 expandingInputTitle.addEventListener('input', function() {
   this.style.height = 'auto';
 
   this.style.height = this.scrollHeight + 'px';
 });
 
+// Adding event listener to the article input field so its height expands and the user types.
 expandingInputArticle.addEventListener('input', function() {
-  this.style.height = 'auto';
-
+  // reset hieght to auto
+  this.style.height = 'auto';  
+  // set the height based on the contents scroll height
   this.style.height = this.scrollHeight + 'px';
 });
 
+// Firebase configuration for initiallising Firebase App and firestore
 const firebaseConfig = {
   apiKey: "AIzaSyC2GQhqdMkLxVmcRMhbu2MyukMhjk7_rI8",
   authDomain: "she-shoot-she-scores.firebaseapp.com",
@@ -25,20 +29,23 @@ const firebaseConfig = {
   measurementId: "G-4VSGK7B0K4"
 };
 
+// initilise firestore for using the database
   firebase.initializeApp(firebaseConfig);
   const db = firebase.firestore();
 
 
-
+// referencing various elements HTML doc. 
 const modalWrapper = document.querySelector('.modal-wrapper');
 const addModal = document.querySelector('.add-modal');
 const addModalForm = document.querySelector('.add-modal .form');
-
-
 const btnAdd = document.querySelector('.btn-add');
 
+// Variable to store the selected article type. 
+// Initially there is no selected article tyoe. 
 let selectedArticleType = "";
 
+
+// Event listeners ti set the selected article type based on which button the user submits with.
 document.getElementById("playersArticles").addEventListener("click", function() {
 selectedArticleType = this.getAttribute("data-type");
 });
@@ -55,13 +62,16 @@ document.getElementById("transfersArticles").addEventListener("click", function(
 selectedArticleType = this.getAttribute("data-type");
 });
 
+//Event listener for when the form in being submitted.
 addModalForm.addEventListener('submit', e => {
   e.preventDefault();
 
   const articleValue = addModalForm.Article.value;
 
+  // Data integrity condition. 
   if (articleValue.length >= 2500) {
 
+    // if the article is atleast 2500 characters long, the article is added to the selected article type collection.
     db.collection(selectedArticleType).add({
       FirstName: addModalForm.FirstName.value,
       LastName: addModalForm.LastName.value,
@@ -70,6 +80,7 @@ addModalForm.addEventListener('submit', e => {
       Article: addModalForm.Article.value,
     })
     .then (() => {
+      // it is then also added to the UserArticles collection in the database.
       db.collection('UserArticles').add({
         FirstName: addModalForm.FirstName.value,
         LastName: addModalForm.LastName.value,
@@ -78,11 +89,13 @@ addModalForm.addEventListener('submit', e => {
         Article: addModalForm.Article.value,
       })
       .then (()=> {
+        //success message
         alert("Thank You For Your Article!")
       })
     })
 
   } else {
+    // display error message if the article is too short. 
     alert("Article must be at least 2500 characters long.")
   }
 
